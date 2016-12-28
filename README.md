a5ad055e1c6b7798174ce8c9bda89c3427e6f77a
Z-Ferguson <zfergusonn@gmail.com>
2016-12-28 12:55:56 -0500

    made program to be fully functional, added meaningful README

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # game-of-life
+
+Conway's Game of Life is a mathematical representation of the cycle of life as theorized by John Conway. This representation takes into account population density to decide whether an object or 'cell' remains 'alive or dead'. Basic parameters are that if there are two or less live neighbors, then the cell in question dies from "lack of support". On the other side, if a cell is bordered by four or more live neighbors, that cell dies from "overpopulation".
+
+## Instructions
+* Open the game_of_life.html, which will bring up a blank screen with two buttons.
+* Click on the Create Table button to draw the table that the cells will populate.
+* You can then click on any arrangement of cells to make any pattern you want while the program isn't running.
+* After you select the arrangement you'd like, click on the 'Toggle Timer' button to have the simulation run.
+* To stop and adjust the table, hit the 'Toggle Timer' button again and make adjustments.
--- js/game_of_life.js
+++ js/game_of_life.js
@@ -1,118 +1,65 @@
-// // function to input amount of rows/columns before game starts
-// var counter = 1;
-// var limit = 2;
-// function addInput(divName){
-//      if (counter == limit)  {
-//           alert("You have reached the limit of adding " + counter + " inputs");
-//      }
-//      else {
-//           var newdiv = document.createElement('div');
-//           newdiv.innerHTML = "Entry " + (counter + 1) + " <br><input type='text' name='myInputs[]'>";
-//           document.getElementById(divName).appendChild(newdiv);
-//           counter++;
-//      }
-// }
-
-
-
-function createTable(){
-    current_seconds = 0
-    var rows = 10
-    var cols = 20
-
-    var $table = $("<table>")
-    $table.attr("id", "lifeboard")
-    $("#gameboard").empty()
-    $("#gameboard").append($table)
-    for(var row = 0; row < rows; row++){
-        var $row = $("<tr>")
-        $table.append($row)
-        console.log($row)
-        for(var col = 0; col < cols; col++){
-            var $col = $("<td>")
-            $col.click(addLife)
-            $row.append($col)
-            $col.attr("id", row + "_" + col)
-        }
-    }
+function insertLife(){
+    $(this).addClass("alive")
 }
 
-function addLife(){
-
+function toggleTime(){
     if(!clock){
-        clock = setInterval(updateClock, 1000)
+        clock = setInterval(lifeDeath, 500)
     }
-
-    // console.log(this)
-    $(this).toggleClass("livecell")
-}
-
-// function rightClick(event){
-//     console.log(this)
-//     event.preventDefault()
-//     $(this).toggleClass("deadcell")
-// }
-
-var clock
-var current_seconds = 0
-function updateClock(){
-    $("#clock").html(current_seconds++)
-}
-
-function gameTick(){
-    if(!clock){
-        clock = setInterval(lifeDeath, 1000)
+    else{
+        clearInterval(clock)
+        clock = 0
     }
 }
 
-function neighboursOf(cell){
-    console.log(cell.attr("id").split("_"))
+function examineSurroundings(cell){
+    if (!cell.attr("id")){
+        return 0
+    }
     var x = parseInt(cell.attr("id").split("_")[0])
     var y = parseInt(cell.attr("id").split("_")[1])
     var count = 0
-    var neighbors = [$("#" + (x-1) + "_" + (y-1)),
-                     $("#" +  x    + "_" + (y-1)),
-                     $("#" + (x+1) + "_" + (y-1)),
-                     $("#" + (x-1) + "_" +  y),
-                     $("#" + (x+1) + "_" +  y),
-                     $("#" + (x-1) + "_" + (y+1)),
-                     $("#" +  x    + "_" + (y+1)),
-                     $("#" + (x+1) + "_" + (y+1))
-                 ]
-    for(var i = 0; i < neighbors.length; i++) {
-        if (neighbors[i].hasClass("livecell")){
+    var surroundings = [$("#" + (x - 1) + "_" + (y - 1)),
+                        $("#" + (x + 1) + "_" + (y + 1)),
+                        $("#" + (x + 1) + "_" + (y - 1)),
+                        $("#" + (x - 1) + "_" + (y + 1)),
+                        $("#" + (x - 1) + "_" + y),
+                        $("#" + x + "_" + (y - 1)),
+                        $("#" + (x + 1) + "_" + y),
+                        $("#" + x + "_" + (y + 1)),
+                        ]
+    for(var i = 0; i < surroundings.length; i++){
+        if (surroundings[i].hasClass("alive")){
             count++
         }
     }
-    return
+    return count
 }
 
-
 function lifeDeath(){
-    var current_table = $("lifeboard")
-    var next_rows = 10
-    var next_cols = 20
-
-    var $next_table = $("<table>")
-    for(var next_row = 0; next_row < next_rows; next_row++){
-        var $next_row = $("<tr>")
-        $next_table.append($next_row)
-        // console.log($next_row)
-        for(var next_col = 0; next_col < next_cols; next_col++){
-            var $next_col = $("<td>")
-            // $col.click(clickCell)
-            $row.append($next_col)
-            $col.attr("id", next_row + "_" + next_col)
-            var check = neighboursOf($('#' + next_row + '_' + next_cell))
-            if (check == 3){
-                $next_cell.addClass("livecell")
-            } else if (check == 2 && $('#' + next_row + '_' + next_cell).hasClass("livecell")){
-                $next_cell.addClass("livecell")
+    var current_table = $("#lifeboard")
+    var parallel_rows = 30
+    var parallel_col = 30
+    var $parallel_table = $("<table>")
+    for(var parallel_row = 0; parallel_row < parallel_rows; parallel_row++){
+        var $parallel_row = $("<tr>")
+        $parallel_table.append($parallel_row)
+        for(var parallel_cell = 0; parallel_cell < parallel_col; parallel_cell++) {
+            var $parallel_cell = $("<td>")
+            $parallel_row.append($parallel_cell)
+            $parallel_cell.attr("id", parallel_row + "_" + parallel_cell)
+            var live_neighbors = examineSurroundings($('#'+parallel_row + "_" + parallel_cell))
+            $parallel_cell.click(insertLife)
+            if (live_neighbors == 3 ||(live_neighbors == 2 && $('#'+parallel_row + "_" + parallel_cell).hasClass("alive"))){
+                $parallel_cell.addClass("alive")
             }
         }
     }
+    $("#world").empty()
+    $("#world").append($parallel_table)
 }
 
-$("#startButton").click(createTable)
-$("#tickButton").click(gameTick)
+
 var clock = false
+$("#drawButton").click(lifeDeath)
+$("#timeButton").click(toggleTime)
